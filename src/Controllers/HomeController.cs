c4317d547e842ea93fad32097f887827401c9841
Jozef Izso <jozef.izso@gmail.com>
2020-08-05 20:48:33 +0200

    Generate LCD raster from image

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace ImageSky5th.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment env;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(IWebHostEnvironment env, ILogger<HomeController> logger)
+        {
+            this.env = env;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var image = await Image.LoadAsync<Rgb24>($@"{this.env.WebRootPath}\pixel50.png");
+
+            return View(image);
+        }
+    }
+}
---
+++ src/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace ImageSky5th
+{
+    public static class Utils
+    {
+        public static string ToHex(this Rgb24 p)
+        {
+            return ((uint)(p.B | (p.G << 8) | (p.R << 16))).ToString("X6");
+        }
+    }
+}
